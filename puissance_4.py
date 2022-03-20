ca3a5d5582a251bbdee5f8de1b683b6e5f54616d
Pacome Daval <pac.daval63@gmail.com>
2022-03-20 17:30:11 +0100

    say_winner

--- puissance_4.py
+++ puissance_4.py
@@ -47,6 +47,12 @@ def comptage(table):
 
     print("Le joueur 1 a ", point1, "points")
     print("Le joueur 2 a ", point2, "points")
+    if point1 > point2:
+        print("C'est le joueur 1 qui a gagné")
+    elif point1 <point2 :
+        print("C'est le joueur 2 qui a gagné")
+    else:
+        print("Il y a égalité")
 
 def passage(tour, j1, j2, table):
     coup = int(input("Dans quel colonne voulait vous jouez ? "))
@@ -72,31 +78,32 @@ def passage(tour, j1, j2, table):
             print("Ce ne peux pas être joué. Veuillez choisir une autre colonne")
             possible = True
             passage(tour,j1, j2, table)
-
-y=3
-print("Ce jeux est le jeu du puissance 4. Les regles sont simples: aligné 4 pions de même couleur à la suite")
-print("Le plateu de jeu commence vide")
-table =np.zeros((4,4))
-print(table)
-print("Quels sont les noms des deux joueurs : ")
-j1 ="a"
-j2 = "a"
-while j1 == j2:
-    j1 = input("Le joueurs 1 s'appelle: ") #1 dans le tableau
-    j2 = input("Et le joueurs 2 s'appelle: ") #2 dans le tabelau
-    if j1 == j2:
-        print("Les noms sont les mêmes veuillez recommencer")
-print(j1, ", tu seras noté 1 dans le tableau")
-print(j2, ", tu seras noté 2 dans le tableau")
-tour = rdm.choice([j1, j2])
-print("Suite au tirage au sort, c'est",tour, "qui va commencer à jouer")
-for x in range(16):
-    passage(tour, j1, j2, table)
-    if tour == j1:
-        tour = j2
-        print("C'est à", j2, "de jouer")
-    else :
-        tour = j1
-        print("C'est à", j1, "de jouer")
-comptage(table)
-print("fin du jeu")
+def init():
+    y=3
+    print("Ce jeux est le jeu du puissance 4. Les regles sont simples: aligné 4 pions de même couleur à la suite")
+    print("Le plateu de jeu commence vide")
+    table =np.zeros((4,4))
+    print(table)
+    print("Quels sont les noms des deux joueurs : ")
+    j1 ="a"
+    j2 = "a"
+    while j1 == j2:
+        j1 = input("Le joueurs 1 s'appelle: ") #1 dans le tableau
+        j2 = input("Et le joueurs 2 s'appelle: ") #2 dans le tabelau
+        if j1 == j2:
+            print("Les noms sont les mêmes veuillez recommencer")
+    print(j1, ", tu seras noté 1 dans le tableau")
+    print(j2, ", tu seras noté 2 dans le tableau")
+    tour = rdm.choice([j1, j2])
+    print("Suite au tirage au sort, c'est",tour, "qui va commencer à jouer")
+    for x in range(16):
+        passage(tour, j1, j2, table)
+        if tour == j1:
+            tour = j2
+            print("C'est à", j2, "de jouer")
+        else :
+            tour = j1
+            print("C'est à", j1, "de jouer")
+    comptage(table)
+    print("fin du jeu")
+init()
